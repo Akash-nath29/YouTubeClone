43681dc84fa78d9b6813542ce051fe8e6b9d2186
Akash-nath29 <anath5440@gmail.com>
2023-10-07 00:11:38 +0530

    created youclone

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SECRET_KEY'] = 'MLXH243rjBDIBibiBIbibIeffi43BBkkfbp15WS7FDhdwYF56wPj8'
+
+db = SQLAlchemy(app)
+class Videos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text(5000), nullable = True)
+    video = db.Column(db.BLOB)
+    
+    def __init__(self, title, description, video):
+        self.title = title
+        self.description = description
+        self.video = video
+
+@app.route('/')
+def index():
+    videos = Videos.query.all()
+    return render_template('index.html', videos=videos)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        video = request.files['videoUpload']
+        title = request.form.get('videoTitle')
+        description = request.form.get('videoDescription')
+        newVideo = Videos(title=title, description=description, video=video.read())
+        db.session.add(newVideo)
+        db.session.commit()
+        return redirect('/')
+    return render_template('upload.html', videos=False)
+
+@app.route('/playVidep/<int:id>')
+def playVideo(id):
+    video = Videos.query.get(id)
+    if video:
+        return render_template('play.html', video=video)
+    else:
+        return "Video not found", 404
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='80')
